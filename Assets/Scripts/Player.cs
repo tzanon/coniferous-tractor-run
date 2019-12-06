2041814880c28f469b3c89a8e3904c602db5c98c
Tyler Zanon <tdzanon@gmail.com>
2019-12-05 21:18:31 -0500

    started on player input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+	private int numApples;
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+	
+	public void Move(InputAction.CallbackContext context)
+	{
+		Vector2 val = context.ReadValue<Vector2>();
+		
+	}
+}
